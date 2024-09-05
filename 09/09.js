6e82afcff06fbe321d03fb90a8f3039386e15e27
jinwook525 <jinwook525@gmail.com>
2024-09-05 17:59:25 +0900

    js

--- 09/09.js
+++ 09/09.js
@@ -1,15 +1,52 @@
 document.addEventListener('DOMContentLoaded', ()=> {
+  //요소 가져오기
   const txt1 =document.querySelector("#txt1");
   const txt2 =document.querySelector("#txt2");
   const bt1= document.querySelector("#bt1");
   const bt2= document.querySelector("#bt2");
 
+ 
+ 
+  //회문
+  bt1.addEventListener("click", (e)=>{
+    e.preventDefault();
+    let s1 = txt1.value.replaceAll(' ',''); 
+    let s2 = "";
 
-  txt1.addEventListener("input", ()=>{
-
-  })
-
-  bt1.addEventListener("click", ()=>{
+    /* for(let i = s1.length -1 ; i >=0 ; i--){
+      s2= s2+s1[i] ;
+    } */
     
-  })
-})
+    s2= s1.split('').reverse().join('');
+      console.log(s1);
+      console.log(s2);
+
+    if (s1 == s2) txt2.value = "회문입니다.";
+    else txt2.value= "회문이 아닙니다."
+  });
+  bt2.addEventListener("click", (e)=>{
+    e.preventDefault();
+      let total=0;
+      let cnt=0;
+      for(let c of txt1.value) {
+        //if(c >= "0" && c <= "9")
+        if(!isNaN(c)){
+          total= total+ parseInt(c);
+          cnt++ ; 
+        }
+      }
+    
+    
+    
+    if (cnt==0) {
+      txt2.value= "숫자가 존재하지 않습니다";
+    } else {
+      txt2.value = total;
+    }
+
+  });
+  //숫자
+ 
+
+});
+ 
